8f8195058c72baacb4f9cc859cc70746e52b3545
nnchaudhuri <nikhilnchaudhuri@gmail.com>
2024-10-07 17:52:31 +0200

    Update breeze.js

--- breeze.js
+++ breeze.js
@@ -139,7 +139,7 @@ class Cell extends Element {
         this.mesh.translate(new BABYLON.Vector3(0, elevT, 0), 1, BABYLON.Space.WORLD);
 
         //setup
-        this.setupVisuals([1, 1, 1], 0.01);
+        this.setupVisuals([1, 1, 1], 0.05);
         this.genLabel();
     }
 
@@ -181,7 +181,7 @@ class Space extends Element {
         this.mesh = BABYLON.Mesh.MergeMeshes(cells.map((cell) => cell.mesh), false, true, undefined, false, false);
 
         //setup
-        this.setupVisuals([1, 1, 1], 0.75);
+        this.setupVisuals([1, 1, 1], 0.5);
     }
 }
 
@@ -300,6 +300,50 @@ class Zone {
             };
         });
     }
+
+    //color zone spaces
+    color() {
+        const n = this.spaces.length;
+        const step = 360/n;
+        
+        for (let i = 0; i < n; i++) {
+            const hue = i*step;
+            const rgb = hslToRgb(hue, 100, 50);
+            this.spaces[i].mesh.material.diffuseColor = new BABYLON.Color3(rgb[0], rgb[1], rgb[2]);
+        }
+    }
+}
+
+//convert HSL to RGB
+function hslToRgb(h, s, l) {
+    s /= 100;
+    l /= 100;
+
+    const c = (1-Math.abs(2*l-1))*s;
+    const x = c*(1-Math.abs(((h/60)%2)-1));
+    const m = l-c/2;
+
+    let r, g, b;
+
+    if (h < 60) {
+        r = c; g = x; b = 0;
+    } else if (h < 120) {
+        r = x; g = c; b = 0;
+    } else if (h < 180) {
+        r = 0; g = c; b = x;
+    } else if (h < 240) {
+        r = 0; g = x; b = c;
+    } else if (h < 300) {
+        r = x; g = 0; b = c;
+    } else {
+        r = c; g = 0; b = x;
+    }
+
+    return [
+        Math.round((r+m)*255),
+        Math.round((g+m)*255),
+        Math.round((b+m)*255)
+    ];
 }
 
 //define minheap class (priority queue returning lowest value node)
@@ -487,7 +531,7 @@ const createScene = async function () {
     //test code
     ///*
         const zone = new Zone(scene, 800);
-        zone.load();
+        zone.load().then(() => {zone.color()});
     //*/
 
     //render updates
